094d98895b1c21aadb53749d59c5f5c24e6fafc9
Amin Ahmadzadeh <s.ahmadzadeh@emofid.com>
2024-11-29 19:14:46 +0330

    init

---
+++ src/main/java/sharif/selab/profiling/Utils.java
@@ -0,0 +1,13 @@
+package sharif.selab.profiling;
+
+public class Utils {
+
+    public static void sleepUninterrruptable(double seconds) {
+        try {
+            Thread.sleep((long)(seconds * 1000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Sleep thread interrupted !");
+        }
+    }
+
+}
